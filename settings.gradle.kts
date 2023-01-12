0ca41724368be9bff5fbb0c5363b9200e3caba77
GitHub Action <action@github.com>
2023-01-12 20:45:04 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "crud-idea-plugin"
--- src/main/kotlin/com/github/gitbic/crudideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gitbic/crudideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gitbic.crudideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gitbic.crudideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
